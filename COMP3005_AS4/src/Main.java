39c722e127ecbc1b639e4186b5e1aefef8b5bd77
JayYu052 <113650269+JayYu052@users.noreply.github.com>
2023-11-23 06:07:16 -0500

    Update Main.java

--- COMP3005_AS4/src/Main.java
+++ COMP3005_AS4/src/Main.java
@@ -2,19 +2,19 @@ import java.sql.Date;
 
 public class Main {
     public static void main(String[] args) {
-        StudentApp dao = new StudentApp();
+        StudentApp Jay = new StudentApp();
 
         // Add a new student
 
-        //dao.addStudent("YUNTAO", "YU", "yuntaoyu@gmail.com", Date.valueOf("2000-05-27"));
+        Jay.addStudent("YUNTAO", "YU", "yuntaoyu@gmail.com", Date.valueOf("2000-05-27"));
 
         // Update a student's email
-        dao.updateStudentEmail(2, "jayjay@gmail.com");
+        Jay.updateStudentEmail(2, "jayjay@gmail.com");
 
         // Delete a student
-        dao.deleteStudent(3);
+        Jay.deleteStudent(3);
 
         // Get all students
-        dao.getAllStudents();
+        Jay.getAllStudents();
     }
 }
